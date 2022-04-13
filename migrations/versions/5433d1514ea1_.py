704dee6e66c97271b7893152a66c9261a3e2f8e8
IsaacH <Isaac.hathaway28@gmail.com>
2022-04-13 15:10:19 -0600

    make css edit on welcome page

---
+++ migrations/versions/5433d1514ea1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5433d1514ea1
+Revises: 8095e966f3f2
+Create Date: 2022-04-13 14:15:03.091101
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5433d1514ea1'
+down_revision = '8095e966f3f2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
